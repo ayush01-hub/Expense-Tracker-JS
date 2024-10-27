b34454c61d9463a2d8a4ca4f8277a5ab5ccea880
Ayush Rastogi <arastogi5422@gmail.com>
2024-10-27 15:22:57 +0530

    script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
+const expenseForm = document.getElementById('expenseForm')
+const expenseList = document.getElementById('expenseList')
 
-        const expenseForm = document.getElementById('expenseForm')
-        const expenseList = document.getElementById('expenseList')
 
-        expenseForm.addEventListener('submit',function(event){
-            event.preventDefault();
+expenseForm.addEventListener('submit',function(event){
+        event.preventDefault();
 
-            const description = document.getElementById('description').value;
-            const category = document.getElementById('category').value;
-            const amount = document.getElementById('amount').value;
+        const description = document.getElementById('description').value;
+        const category = document.getElementById('category').value;
+        const amount = document.getElementById('amount').value;
 
-            if(document && category && !isNaN(amount)){
+        if(document && category && !isNaN(amount)){
                 const newRow = document.createElement('tr')
 
                 newRow.innerHTML = `<td>${description}</td>
@@ -19,12 +19,9 @@
                 expenseList.appendChild(newRow)
 
                 document.getElementById('description').value=''
-            document.getElementById('category').value=''
-            document.getElementById('amount').value=''
-            }else{
+                document.getElementById('category').value=''
+                document.getElementById('amount').value=''
+        }else{
                 alert('Please fill out all fields with valid Data')
-            }
-
-            
-
-        })
+        }
+ })
